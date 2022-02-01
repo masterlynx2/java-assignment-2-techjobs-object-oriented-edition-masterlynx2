2f8067b7ec1abef65a52824d9f0da6f4394859c0
Masterlynx2 <Tonywatkins325@gmail.com>
2022-02-01 14:27:24 -0500

    step 4 completed for test cases

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -17,7 +17,7 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
-    public Location(int id, String value) {
+    public Location( String value) {
         this();
         this.value = value;
     }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,74 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
 
+import static org.junit.Assert.*;
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
+
+
+
 public class JobTest {
+
+    //   @Before
+//        Job a1= new Job("performer","launchcode","detroit","singer","vocals");
+
+    Job a2;
+    Job a3;
+    Job tester;
+    Job tester2;
+
+    @Before
+    public void Job() {
+        a2 = new Job();
+        a3 = new Job();
+        tester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        tester2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertFalse(a2.getId() == a3.getId());
+    }
+
+//    @Test
+//    public void UniqueIDTest1() {
+//        assertEquals(1, a2.getId(), .001);
+//
+//
+//    }
+//
+//    @Test
+//    public void UniqueIDTest2() {
+//        assertEquals(2, a2.getId(), .001);
+//    }
+//
+//    @Test
+//    public void UniqueIDTest3() {
+//        assertEquals(3, a2.getId(), .001);
+//
+//    }
+
+    @Test
+
+    public void testJobConstructorSetsAllFields() {
+
+        assertEquals(true, tester != null);
+
+    }
+
+    @Test
+
+    public void testJobsForEquality(){
+        assertFalse(tester instanceof Job!=tester2 instanceof Job);
+
+    }
 
 }
