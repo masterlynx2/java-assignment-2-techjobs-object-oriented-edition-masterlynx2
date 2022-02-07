2bd6b807232fb239680ca2d43fecc146579ecae6
Masterlynx2 <Tonywatkins325@gmail.com>
2022-02-07 11:37:49 -0500

    step 6  refactored code abstract class created and related jobfield classes assigned

--- src/main/java/org/launchcode/techjobs/oo/test/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobField.java
@@ -1,5 +1,49 @@
 package org.launchcode.techjobs.oo.test;
 
-public class JobField {
+import java.util.Objects;
 
+public abstract class JobField {
+
+    private int id;
+    private static int nextID = 1;
+    private String value;
+
+    public JobField(){
+        id =nextID;
+        nextID++;
+    }
+
+    public JobField(String value){
+        this();
+        this.value = value;
+    }
+
+    public String toString(){
+        return this.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
